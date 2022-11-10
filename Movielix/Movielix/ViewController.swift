e9bb9dbd4e0bdd84308764055851240223cfdb78
Banut Raul <banut.raul@yahoo.com>
2022-11-10 14:39:09 +0200

    First commit

---
+++ Movielix/Movielix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movielix
+//
+//  Created by Raul Banut on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
